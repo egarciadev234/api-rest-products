1c85fb11c4961313d73938e4b4c41a8da1b9f90b
erick <developeeg@gmail.com>
2018-10-12 09:13:16 -0500

    Revert "apirest"
    This reverts commit dc8b2abb46c4f220cdec93525bdf614374701939.

--- producto/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Producto
-class ProductoSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Producto
-		fields = ('title', 'description')
-
--- producto/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import *
-urlpatterns = [
-    path('productos', ListProduct.as_view(), name='list-productos'),
-    path('productos/<int:pk>', DetailProduct.as_view(), name='detail-producto'),
-]
